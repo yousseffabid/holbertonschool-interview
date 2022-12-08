411176b3389a4308dcedd5237139715e1c7bc5de
yousseffabid <abidyoussef58@gmail.com>
2022-12-08 15:20:23 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
 function that checks if all boxes can be unlocked
-A key with the same number as a box opens that box
-You can assume all keys will be positive integers
-There can be keys that do not have boxes
-The first box boxes[0] is unlocked
-Return True if all boxes can be opened, else return False
 """
 
 
@@ -14,8 +9,10 @@ def canUnlockAll(boxes):
     number_boxes = len(boxes)
     keys = boxes[0]
     box_locked = [False] + [True] * (number_boxes - 1)
+
     for key in keys:
         if ((key < number_boxes) and (box_locked[key] is True)):
             box_locked[key] = False
             keys.extend(boxes[key])
+
     return not any(box_locked)
