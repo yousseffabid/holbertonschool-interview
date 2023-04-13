450fd0ced56c422c816f84de5ebb53cc5b6e4a41
yousseffabid <abidyoussef58@gmail.com>
2023-04-13 20:10:27 +0000

    starwars chracters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', async (err, res, body) => {
-  if (err) {
-    return console.error(err);
-  }
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
+// Makes API request, sets async to allow await promise
+request(filmURL + filmNum, async function(err, res, body) {
+  if (err) return console.error(err);
+
+  // find URLs of each character in the film as a list obj
   const urlList = JSON.parse(body).characters;
 
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
   for (const charURL of urlList) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, res, body) => {
-        if (err) {
-          return console.error(err);
-        }
+    await new Promise(function(resolve, reject) {
+      request(charURL, function(err, res, body) {
+        if (err) return console.error(err);
 
+        // finds each character name and prints in URL order
         console.log(JSON.parse(body).name);
         resolve();
       });
