397ac3b1814d9390c2f15468380cd3ce0f3a6b9c
yousseffabid <abidyoussef58@gmail.com>
2023-04-13 20:06:58 +0000

    starwars chracters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,35 +1,23 @@
 #!/usr/bin/node
-// using Star Wars API, prints all `characters` of a given film, in order
 const request = require('request');
 
-function getCharName(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(JSON.parse(body).name);
-    });
-  });
-}
-
-async function charsInFilm(urlList) {
-  try {
-    let name;
-    for (const url of urlList) {
-      name = await getCharName(url);
-      console.log(name);
-    }
-  } catch (error) {
-    console.error(error);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', async (err, res, body) => {
+  if (err) {
+    return console.error(err);
   }
-}
 
-const filmsURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(filmsURL, function(error, response, body) {
-  if (error) {
-    console.error(error);
-  }
   const urlList = JSON.parse(body).characters;
-  charsInFilm(urlList);
+
+  for (const charURL of urlList) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, res, body) => {
+        if (err) {
+          return console.error(err);
+        }
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
